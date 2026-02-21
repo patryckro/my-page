2739511747b6a604578ecd999a51040ca708d795
Patryck Moulaz <patryckmoulaz@gmail.com>
2026-02-20 22:46:19 -0300

    feat: github actions ci/cd

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const withNextIntl = createNextIntlPlugin('./i18n.ts');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = withNextIntl(nextConfig);
